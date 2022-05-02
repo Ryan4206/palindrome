e5a8a577e973e06794170e490ebdcbd1bbfc5b9f
Ryan4206 <100150618+Ryan4206@users.noreply.github.com>
2022-05-02 12:07:35 -0400

    Initial upload

---
+++ Palindrome.py
@@ -0,0 +1,19 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+text = "racecar"
+for i in range (len(text)):
+    j = -1 - i
+    if text[i] == text[j]:
+        pass
+    else:
+        print(False)
+        break
+print(True)
+
+
+            
+            
+    
+
+        
+        
